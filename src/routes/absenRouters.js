b2887be68f256f6252fe83e2dcdc15537f8c048b
Asterix627 <alwikhafi27@gmail.com>
2024-06-11 23:05:24 +0700

    delete=routerabsen

--- src/routes/absenRouters.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const verifyToken = require('../middleware/auth');
-const { absenMasukController } = require('../controllers/absenMasuk');
-
-router.put('/absen-masuk', verifyToken, absenMasukController);
-
-module.exports = router;
